49408efd357e8a7e82f04024c96cfb0b5e00e07c
Gabriel-affont <162576104+Gabriel-affont@users.noreply.github.com>
2026-09-22 22:00:28 +0300

    Github pages deployment

--- Fee-calculator/next.config.mjs
+++ Fee-calculator/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+  images: { unoptimized: true },
+  basePath: "/KIRINYAGA-FEE-LEDGER/Fee-calculator",
+  assetPrefix: "/KIRINYAGA-FEE-LEDGER/Fee-calculator/",
+};
+
 
 export default nextConfig;
